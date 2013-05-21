b1809a088b75ae4516c97ea4b36b94da2f7bb2b3
Ben Colon <ben@colon.com.fr>
2013-05-21 17:41:33 +0200

    Some some tab spaces and remove a Rails method

--- lib/dashing_storage.rb
+++ lib/dashing_storage.rb
@@ -5,9 +5,9 @@ require "dashing_storage/version"
 
 module DashingStorage
   def self.db
-  	return @db if @db
+    return @db if @db
     @db = connection.db(config['database'])
-    @db.authenticate(config['user'], config['password']) unless (config['user'].nil? || config['user'].blank?)
+    @db.authenticate(config['user'], config['password']) unless (config['user'].nil? || config['user'].empty?)
     @db
   end
 
@@ -18,11 +18,11 @@ module DashingStorage
   end
 
   def self.config
-  	yml = YAML.load_file('database.yml')
-  	yml[env]
+    yml = YAML.load_file('database.yml')
+    yml[env]
   end
 
   def self.env
-  	Sinatra::Application.environment.to_s
+    Sinatra::Application.environment.to_s
   end
 end
